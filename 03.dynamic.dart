599f9aa4db37c3f77d56f395f01fdacd879159e5
Ricardo Metzger <r7metzger@gmail.com>
2024-11-15 10:30:00 -0500

    feat: dynamic en dart

---
+++ 03.dynamic.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  dynamic myVariable = 'Hola';
+  print(myVariable);
+  myVariable = 1234;
+  print(myVariable);
+  myVariable = false;
+  print(myVariable);
+  myVariable = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  print(myVariable);
+  myVariable = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  print(myVariable);
+  myVariable = () => true;
+  print(myVariable);
+  myVariable = null;
+  print(myVariable);
+}
