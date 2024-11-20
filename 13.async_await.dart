78012e1e227d860916720814ab1107079d561e22
Ricardo Metzger <r7metzger@gmail.com>
2024-11-19 21:36:26 -0500

    feat: async, await en dart

---
+++ 13.async_await.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) async {
+  print('inicio del programa');
+  String responseGet = await httpGet('https://www.algo.com');
+  print(responseGet);
+  try {
+    String responseGetError = await httpGetError('https://www.algo.com');
+    print(responseGetError);
+  } catch (e) {
+    print('Tenemos un error: $e');
+  }
+  print('fin del programa');
+}
+
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+  return 'Valor de la petición';
+}
+
+Future<String> httpGetError(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+  throw 'Error en la petición';
+}
